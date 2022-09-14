5748762a5ca5c80787d96b2077f20a30a3d1975b
mhbj2000 <91085238+mhbj2000@users.noreply.github.com>
2022-09-14 15:59:28 -0400

    Update GANS_DEMO_0.py

--- GANS_DEMO_0.py
+++ GANS_DEMO_0.py
@@ -36,7 +36,7 @@ with st.container():
     with st.expander("Metrics and Results"):
         col1, col2 = st.columns(2)
         with col1:
-            #st.image("https://static.streamlit.io/examples/dice.jpg")
+            st.image("https://static.streamlit.io/examples/dice.jpg")
             
         with col2:
             st.subheader("Metrics")
