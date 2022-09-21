5c73fc034a5173be47aff47b4896c56c634f0a36
mhbj2000 <91085238+mhbj2000@users.noreply.github.com>
2022-09-21 16:41:15 -0400

    Update GANS_DEMO_0.py

--- GANS_DEMO_0.py
+++ GANS_DEMO_0.py
@@ -43,17 +43,6 @@ with st.container():
         if st.sidebar.button("Clear Input"):
             picResult = []
         st.sidebar.button("Stop")
-    with st.expander("Metrics and Results"):
-        col1, col2 = st.columns(2)
-        with col1:
-            st.subheader("Metrics")
-            st.write(seedData , "real images")
-            st.write(synData ,"synthetic images")
-            GANData = seedData*synData
-            st.write(GANData,"GAN images")
-            st.write("Generated in - minutes and - seconds")
-        with col2:
-            st.image("https://static.streamlit.io/examples/dice.jpg")
             
 with st.container():
     col1, col2, col3, col4, col5, col6= st.columns(6)
