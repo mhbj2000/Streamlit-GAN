2403e1c35cdb993dca9814f54fdf3918a87ebd4d
GivanteLewis <112026471+GivanteLewis@users.noreply.github.com>
2022-09-27 11:28:10 -0400

    Update GALLERY.py

--- pages/GALLERY.py
+++ pages/GALLERY.py
@@ -57,9 +57,6 @@ def picGen():
 with st.container():
     st.sidebar.subheader("CONTROLS")
     #st.slider("Slider tester", 1, 5000, 2000)
-    seedData = st.sidebar.slider("Seed Data",1,5,5)
-    synData = st.sidebar.slider("Synthetic Data",1,5,5)
-    GANData = seedData*synData
     left_column, right_column = st.sidebar.columns(2)
     with left_column:
         if st.sidebar.button("Start Gallery"):
