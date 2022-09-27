bd2babb456d83ddef6b2736edea8a095eb9eaf95
GivanteLewis <112026471+GivanteLewis@users.noreply.github.com>
2022-09-27 11:25:48 -0400

    Update GALLERY.py

--- pages/GALLERY.py
+++ pages/GALLERY.py
@@ -48,7 +48,7 @@ def picGen(pictures):
     fileTypeName = ".png"
     newsize = (200, 200)
     picTest_list = []
-    for i in range(1,26):
+    for i in range(1,25):
        # st.write(fileName + str(i)+fileTypeName)
         pic_test = Image.open(fileName+str(i)+fileTypeName)
         pic_test = pic_test.resize(newsize)
@@ -62,47 +62,13 @@ with st.container():
     GANData = seedData*synData
     left_column, right_column = st.sidebar.columns(2)
     with left_column:
-        if st.sidebar.button("Generate GANs"):
+        if st.sidebar.button("Start Gallery"):
             picResult = picGen(GANData)
             picDisplay(picResult)
-        st.sidebar.button("Randomize Synthetic Data") 
+        #st.sidebar.button("Randomize Synthetic Data") 
     with right_column:
-        if st.sidebar.button("Clear Input"):
-            picResult = []
-        st.sidebar.button("Stop")
+        #if st.sidebar.button("Clear Input"):
+            #picResult = []
+        #st.sidebar.button("Stop")
  
-with st.container():
-    #image container
-    st.write("---")
-    col1, col2, col3, col4, col5, col6 = st.columns(6)
-    def picDisplay(picResult):
-        
-        nextcol = 1
-        newsize = (500,500)
-        for i in range(len(picTest_list)):
-            picture_result = picTest_list[i].resize(newsize)
-            if(nextcol == 6):
-                with col6:
-                    st.image(picture_result)
-                    nextcol = 1
-            elif (nextcol == 5):
-                with col5:
-                    st.image(picture_result)
-                    nextcol = 6
-            elif (nextcol == 4):
-                with col4:
-                    st.image(picture_result)
-                    nextcol = 5
-            elif (nextcol == 3):
-                with col3:
-                    st.image(picture_result)
-                    nextcol = 4
-            elif (nextcol == 2):
-                with col2:
-                    st.image(picture_result)
-                    nextcol = 3
-            elif (nextcol == 1):
-                with col1:
-                    st.image(picture_result)
-                    nextcol = 2
-                   
+
