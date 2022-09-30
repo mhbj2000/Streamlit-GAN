55232fb18bca30b91d4ac822cd673671474cb593
mhbj2000 <91085238+mhbj2000@users.noreply.github.com>
2022-09-30 09:55:46 -0400

    Update START_HERE.py

--- START_HERE.py
+++ START_HERE.py
@@ -14,7 +14,17 @@ with st.container():
     st.markdown('##')
 
     st.write("A generative adversarial network (GAN) is a deep neural network that is trained with existing training data to generate new data of similar characteristics to the training data. For a GAN to work it needs two neural networks the generator, which is trained to produce fake data, and the discriminator, which is trained to distinguish the generator’s fake data from real examples. If the fake data generated is easily recognized as implausible by the discriminator, then the generator is penalized and must try again until it generates something plausible.  ")
+    
+    st.markdown("""
+    <style>
+    .big-font {
+        font-size:300px !important;
+    }
+    </style>
+    """, unsafe_allow_html=True)
 
+    st.markdown('<p class="big-font">Hello World !!</p>', unsafe_allow_html=True)
+    
 with st.container():
     col1, col2, col3, col4, col5 = st.columns(5)
     with col3:
