ed0e38ad74bc6130ef1da795751eaf57142b47fc
GivanteLewis <112026471+GivanteLewis@users.noreply.github.com>
2022-09-30 09:51:57 -0400

    Update GALLERY.py

--- pages/GALLERY.py
+++ pages/GALLERY.py
@@ -6,7 +6,8 @@ from PIL import Image
 
 st.set_page_config(page_title="Gallery", layout="wide")
 pic_list = []
-global start_index = 1
+global start_index 
+start_index = 1
 with st.container():
     #image container
     st.write("---")
