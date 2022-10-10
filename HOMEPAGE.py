534833203f959f49cc77057dd7000ac9a47c888b
mhbj2000 <91085238+mhbj2000@users.noreply.github.com>
2022-10-10 14:59:37 -0400

    Update HOMEPAGE.py

--- HOMEPAGE.py
+++ HOMEPAGE.py
@@ -23,6 +23,12 @@ with st.container():
 
     st.markdown('<p class="big-font">A generative adversarial network (GAN) is a deep neural network that is trained with existing training data to generate new data of similar characteristics to the training data. For a GAN to work it needs two neural networks the generator, which is trained to produce fake data, and the discriminator, which is trained to distinguish the generator’s fake data from real examples. If the fake data generated is easily recognized as implausible by the discriminator, then the generator is penalized and must try again until it generates something plausible.  </p>', unsafe_allow_html=True)
 
+
+with st.container():
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col3:
+        st.image("./wip_photo.png")
+
 with st.container():
     
     st.markdown('##')
@@ -37,7 +43,3 @@ with st.container():
 
     st.markdown('<p class="big-font">The purpose of the discriminator is to identify real data from fake data created by our generator. </p>', unsafe_allow_html=True)
         
-with st.container():
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col3:
-        st.image("./wip_photo.png")
