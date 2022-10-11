aff16d215b9caf9ce417e70417e51da7c967adb9
mhbj2000 <91085238+mhbj2000@users.noreply.github.com>
2022-10-11 00:08:33 -0400

    Update HOMEPAGE.py

--- HOMEPAGE.py
+++ HOMEPAGE.py
@@ -24,12 +24,19 @@ with st.container():
 
     st.markdown('<p class="big-font">A generative adversarial network (GAN) is a deep neural network that is trained with existing training data to generate new data of similar characteristics to the training data. For a GAN to work it needs two neural networks the generator, which is trained to produce fake data, and the discriminator, which is trained to distinguish the generator’s fake data from real examples. If the fake data generated is easily recognized as implausible by the discriminator, then the generator is penalized and must try again until it generates something plausible.  </p>', unsafe_allow_html=True)
 
-
+#HERE IS WHERE GIF GOES
 with st.container():
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col3:
-        st.image("./wip_photo.png")
-
+    file_ = open("/Gear_Turn.gif", "rb")
+    contents = file_.read()
+    data_url = base64.b64encode(contents).decode("utf-8")
+    file_.close()
+    
+    st.markdown(
+        f'<img src="data:image/gif;base64,{data_url}" alt="Gear gif">',
+        unsafe_allow_html=True,
+    )
+    
+    
 with st.container():
     
     st.markdown('##')
