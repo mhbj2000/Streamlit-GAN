320cb4496e158e59709b3166156c2929f21affdd
mhbj2000 <91085238+mhbj2000@users.noreply.github.com>
2022-10-25 11:12:42 -0400

    Update HOMEPAGE.py

--- HOMEPAGE.py
+++ HOMEPAGE.py
@@ -11,28 +11,27 @@ with open("design.css") as source_des:
 st.markdown("<h1 style='text-align: center;'>WHAT IS GANS?</h1>", unsafe_allow_html=True)
 
 with st.container():
-    col1, col2 = st.columns(2)
-    with col1:
-        st.markdown('##')
 
-        st.markdown("""
-        <style>
-        .big-font {
-            font-size:24px !important;
-        }
-        </style>
-        """, unsafe_allow_html=True)
+    st.markdown('##')
 
-        st.markdown('<p class="big-font">A generative adversarial network (GAN) is a deep neural network that is trained with existing training data to generate new data of similar characteristics to the training data. For a GAN to work it needs two neural networks the generator, which is trained to produce fake data, and the discriminator, which is trained to distinguish the generator’s fake data from real examples. If the fake data generated is easily recognized as implausible by the discriminator, then the generator is penalized and must try again until it generates something plausible.  </p>', unsafe_allow_html=True)
+    st.markdown("""
+    <style>
+    .big-font {
+        font-size:24px !important;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+    st.markdown('<p class="big-font">A generative adversarial network (GAN) is a deep neural network that is trained with existing training data to generate new data of similar characteristics to the training data. For a GAN to work it needs two neural networks the generator, which is trained to produce fake data, and the discriminator, which is trained to distinguish the generator’s fake data from real examples. If the fake data generated is easily recognized as implausible by the discriminator, then the generator is penalized and must try again until it generates something plausible.  </p>', unsafe_allow_html=True)
 
 #HERE IS WHERE GIF GOES
-    with col2:
-        file_ = open("DiscriminatorAnim.gif", "rb")
-        contents = file_.read()
-        data_url = base64.b64encode(contents).decode("utf-8")
-        file_.close()
+with st.container():
+    file_ = open("DiscriminatorAnim.gif", "rb")
+    contents = file_.read()
+    data_url = base64.b64encode(contents).decode("utf-8")
+    file_.close()
 
-        st.markdown(f'<img src="data:image/gif;base64,{data_url}" alt="Gear gif">',unsafe_allow_html=True,)
+    st.markdown(f'<img src="data:image/gif;base64,{data_url}" alt="Gear gif">',unsafe_allow_html=True,)
 
 
 
