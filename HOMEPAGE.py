f8d6c144c94073357f24bb0f54146dc8c3c1c138
mhbj2000 <91085238+mhbj2000@users.noreply.github.com>
2022-10-25 09:34:42 -0400

    Update HOMEPAGE.py

--- HOMEPAGE.py
+++ HOMEPAGE.py
@@ -5,10 +5,11 @@ st.set_page_config(page_title="GANS", layout="wide")
 
 #content of main_page
 
-with st.container():
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col3:
-        st.title("HOW IT WORKS")
+st.markdown("<h1 style='text-align: center;'>HOW IT WORKS</h1>", unsafe_allow_html=True)
+#with st.container():
+    #col1, col2, col3, col4, col5 = st.columns(5)
+    #with col3:
+        #st.title("HOW IT WORKS")
 
 with st.container():
     
