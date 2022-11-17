e46fada604ff97497c531653b70369e5bcf31f1d
GivanteLewis <112026471+GivanteLewis@users.noreply.github.com>
2022-11-17 16:05:12 -0500

    Update 2_GANS.py

--- pages/2_GANS.py
+++ pages/2_GANS.py
@@ -33,7 +33,7 @@ with st.container():
     with col3: #CONTROLS FOR THE ENTIRE ROW
         model_selector = st.selectbox('Choose an image to run.',('Example 1', 'Example 2', 'Example 3'))
         if model_selector == 'Example 1':
-            rain_counter = st.slider('GAN Iteration', 0,9,0)
+            rain_counter = st.slider('Intermediate Image Number', 0,9,0)
             true_counter = rain_counter + 40
             rain_timelapse_picture = Image.open('IntermediatePic/RainFake/Fake4/False_image_'+str(true_counter)+'.png')
             image_resizer = (400,400)
