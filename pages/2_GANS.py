77df69837e8fe5be11f957bcd8a6435791784ab8
GivanteLewis <112026471+GivanteLewis@users.noreply.github.com>
2022-11-17 15:54:11 -0500

    Update 2_GANS.py

--- pages/2_GANS.py
+++ pages/2_GANS.py
@@ -41,7 +41,7 @@ with st.container():
             video_bytes = rain_timelapse.read()
         elif model_selector == 'Example 2':
             #rain_counter = st.slider('GAN Iteration',
-            rain_timelapse = open('Time-lapse/RainTimelapse/Timelapse12.mp4', 'rb')
+            rain_timelapse = open('Time-lapse/RainTimelapse/Timelapse13.mp4', 'rb')
             video_bytes = rain_timelapse.read()
             
         
