6d2b4c47332545b7d170b2dd3ea31190919ed45f
GivanteLewis <112026471+GivanteLewis@users.noreply.github.com>
2022-11-17 23:23:26 -0500

    Update GALLERY.py

--- pages/GALLERY.py
+++ pages/GALLERY.py
@@ -27,27 +27,27 @@ with st.container():
   #picDisplay()
 with st.container():
   col1, col2, col3, col4 = st.columns(4)
-    def picDisplay():
-      image_list = picGen(current_model,current_iteration)
-      nextcol = 1
-      for i in range(len(image_list)):
-        if(nextcol == 4):
-          with col4:
-            st.image(image_list[i])
-            nextcol = 1
-        elif (nextcol == 3):
-          with col3:
-            st.image(image_list[i])
-            nextcol = 4
-        elif (nextcol == 2):
-          with col2:
-            st.image(image_list[i])
-            nextcol = 3
-        elif (nextcol ==1):
-          with col1:
-            st.image(image_list[i])
-            nextcol = 2
-        return 0
+  def picDisplay():
+    image_list = picGen(current_model,current_iteration)
+    nextcol = 1
+    for i in range(len(image_list)):
+      if(nextcol == 4):
+        with col4:
+          st.image(image_list[i])
+          nextcol = 1
+      elif (nextcol == 3):
+        with col3:
+          st.image(image_list[i])
+          nextcol = 4
+      elif (nextcol == 2):
+        with col2:
+          st.image(image_list[i])
+          nextcol = 3
+      elif (nextcol ==1):
+        with col1:
+          st.image(image_list[i])
+          nextcol = 2
+      return 0
 picDisplay()
   
     
