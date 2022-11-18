3dd8ce0b00059589fbb4208983dfdd773341fd7b
GivanteLewis <112026471+GivanteLewis@users.noreply.github.com>
2022-11-17 23:22:19 -0500

    Update GALLERY.py

--- pages/GALLERY.py
+++ pages/GALLERY.py
@@ -21,11 +21,34 @@ with st.container():
       current_image = Image.open('IntermediatePic/'+str(current_model)+'Fake/Fake'+(str(i))+'/False_image_'+str(current_iteration)+'.png')
       current_image = current_image.resize((200,200))
       image_list.append(current_image)
-      st.image(current_image)
       current_iteration = current_iteration + 10
     return image_list
   picGen(current_model,current_iteration)
   #picDisplay()
+with st.container():
+  col1, col2, col3, col4 = st.columns(4)
+    def picDisplay():
+      image_list = picGen(current_model,current_iteration)
+      nextcol = 1
+      for i in range(len(image_list)):
+        if(nextcol == 4):
+          with col4:
+            st.image(image_list[i])
+            nextcol = 1
+        elif (nextcol == 3):
+          with col3:
+            st.image(image_list[i])
+            nextcol = 4
+        elif (nextcol == 2):
+          with col2:
+            st.image(image_list[i])
+            nextcol = 3
+        elif (nextcol ==1):
+          with col1:
+            st.image(image_list[i])
+            nextcol = 2
+        return 0
+  picDisplay()
   
     
     
