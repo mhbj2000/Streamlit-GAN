163128a9731304680dfd497bca6b1a4f2b74f555
GivanteLewis <112026471+GivanteLewis@users.noreply.github.com>
2022-11-17 23:40:46 -0500

    Update GALLERY.py

--- pages/GALLERY.py
+++ pages/GALLERY.py
@@ -13,6 +13,7 @@ with st.container():
     current_model =st.selectbox('Choose a Model',('Rain','Fog'))
   with col2:
     current_iteration = st.selectbox('Choose an Iteration',(0,1,2,3,4,5))
+  st.sidebar.write('Iteration 0 is the Real Image and Iteration 5 is the final GAN image.')
   def picGen(current_model,current_iteration):
     current_iteration = current_iteration
     image_list =[]
