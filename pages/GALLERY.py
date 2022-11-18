265c75aee8c9585e3433060a4ad719f9f291a18d
GivanteLewis <112026471+GivanteLewis@users.noreply.github.com>
2022-11-17 23:36:02 -0500

    Update GALLERY.py

--- pages/GALLERY.py
+++ pages/GALLERY.py
@@ -16,7 +16,6 @@ with st.container():
   def picGen(current_model,current_iteration):
     current_iteration = current_iteration
     image_list =[]
-    #image_string = ('IntermediatePic/'+(current_model)+'Fake/Fake'+(fake_counter)+ 'False_image_'+str(current_iteration)+'.png')
     for i in range(0,20):
       current_image = Image.open('IntermediatePic/'+str(current_model)+'Fake/Fake'+(str(i))+'/False_image_'+str(current_iteration)+'.png')
       current_image = current_image.resize((500,500))
@@ -48,48 +47,4 @@ with st.container():
           st.image(image_list[i])
           nextcol = 2
 picDisplay()
-  
-    
-    
- 
-                                
-  
-    
-
-
-
-
-
-
-#if 'start_index' not in st.session_state:
-    #st.session_state.start_index = 1
-#pic_list = []
-#st.session_state.start_index = 1
-#start_index = 1
-#with st.container():
-    #image container
-    #st.write("---")
-    #col1, col2, col3, col4 = st.columns(4)
-    #def picDisplay():
-      #image_list = picGen(current_model,current_iteration)
-      #nextcol = 1
-      #for i in range(len(image_list)):
-        #if(nextcol == 4):
-          #with col4:
-            #st.image(image_list[i])
-            #nextcol = 1
-        #elif (nextcol == 3):
-          #with col3:
-            #st.image(image_list[i])
-            #nextcol = 4
-        #elif (nextcol == 2):
-          #with col2:
-            #st.image(image_list[i])
-            #nextcol = 3
-        #elif (nextcol ==1):
-          #with col1:
-            #st.image(image_list[i])
-            #nextcol = 2
-        #return 0
-
 
