058c90365599a2b76a849e4d812799030850253b
GivanteLewis <112026471+GivanteLewis@users.noreply.github.com>
2022-11-19 16:01:40 -0500

    Update 2_GANS.py

--- pages/2_GANS.py
+++ pages/2_GANS.py
@@ -52,11 +52,12 @@ with tab1:
     col1,col2,col3 = st.columns(3)
     with col2:
         st.subheader('Image Slider')
-        st.write('The total length of the clear image dataset = 1013 images  \n')
-        st.write('The total length of the rainy image dataset = 1054 images  \n')
-        st.write('The total time to train the clear to rainy model was: 10:30hours')
+        st.markdown('<p class="main-text">Length of Clear Dataset: 1013 Images <br>Length of Rainy Dataset: 1054 Images <br> Time to Train Rain Model: 10:30 Hours</p>', unsafe_allow_html=True)
+        #st.write('The total length of the clear image dataset = 1013 images  \n')
+        #st.write('The total length of the rainy image dataset = 1054 images  \n')
+        #st.write('The total time to train the clear to rainy model was: 10:30hours')
         
-        image_counter = st.slider('Image Counter',1,5,1,key = 'clear_image slider',label_visibility ='collapsed')
+        image_counter = st.slider('Image Display Slider',1,5,1,key = 'clear_image slider',label_visibility ='visible')
         for i in range(12):
             st.markdown('#')
  
