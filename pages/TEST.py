59fc3c5c9d282ebc732324f2698aad5cc8013169
mhbj2000 <91085238+mhbj2000@users.noreply.github.com>
2022-11-19 19:04:05 -0500

    Update TEST.py

--- pages/TEST.py
+++ pages/TEST.py
@@ -1,41 +1,82 @@
 import streamlit as st
 from PIL import Image
-import base64
 
-st.set_page_config(page_title="GANS", layout="wide")
+                   
 
-#content of main_page
 
-#header
-with open("design.css") as source_des:
-    st.markdown(f"<style>{source_des.read()}</style>", unsafe_allow_html=True)
-st.markdown("<h1 style='text-align: center;'>WHAT IS A GAN?</h1>", unsafe_allow_html=True)
-
+st.set_page_config(page_title="Gallery", layout="wide")
+#"st.session_state object:", st.session_state
+pic_list = []
+st.session_state['start_index'] = 1
+#start_index = 1
 with st.container():
+    #image container
+    st.write("---")
+    col1, col2, col3, col4, col5, col6 = st.columns(6)
+    def picDisplay(picResult):
+        
+        nextcol = 1
+        newsize = (500,500)
+        for i in range(len(picResult)):
+            picture_result = picResult[i].resize(newsize)
+            if(nextcol == 6):
+                with col6:
+                    st.image(picture_result)
+                    nextcol = 1
+            elif (nextcol == 5):
+                with col5:
+                    st.image(picture_result)
+                    nextcol = 6
+            elif (nextcol == 4):
+                with col4:
+                    st.image(picture_result)
+                    nextcol = 5
+            elif (nextcol == 3):
+                with col3:
+                    st.image(picture_result)
+                    nextcol = 4
+            elif (nextcol == 2):
+                with col2:
+                    st.image(picture_result)
+                    nextcol = 3
+            elif (nextcol == 1):
+                with col1:
+                    st.image(picture_result)
+                    nextcol = 2
+        return 0
+                   
 
-    st.markdown('##')
-
-    st.markdown("""
-    <style>
-    .big-font {
-        font-size:24px !important;
-    }
-    </style>
-    """, unsafe_allow_html=True)
-
-    st.markdown('<p class="big-font">A generative adversarial network (GAN) is a deep neural network that is trained with existing training data to generate new data of similar characteristics to the training data. For a GAN to work it needs two neural networks the generator, which is trained to produce fake data, and the discriminator, which is trained to distinguish the generator’s fake data from real examples. If the fake data generated is easily recognized as implausible by the discriminator, then the generator is penalized and must try again until it generates something plausible.  </p>', unsafe_allow_html=True)
-
-#HERE IS WHERE GIF GOES
-col1, col2, col3 = st.columns(3)
-with col1:
-    file_ = open("DiscriminatorAnim.gif", "rb")
-    contents = file_.read()
-    data_url = base64.b64encode(contents).decode("utf-8")
-    file_.close()
-
-    st.markdown(f'<img src="data:image/gif;base64,{data_url}" alt="Gear gif">',unsafe_allow_html=True,)
-
+def picGen(start_index):
+    st.write(start_index)
+    fileName = "Pic/Test_"
+    fileTypeName = ".png"
+    newsize = (200, 200)
+    picTest_list = []
+    for i in range(start_index,start_index+12):
+        pic_test = Image.open(fileName+str(i)+fileTypeName)
+        pic_test = pic_test.resize(newsize)
+        picTest_list.append(pic_test)
+    return picTest_list
 with st.container():
-    st.markdown('##')
-    image = Image.open('GAN_SYSTEM.PNG')
-    st.image(image, caption ='Final Animation Demo')
+    st.sidebar.subheader("CONTROLS")
+    left_column, right_column = st.sidebar.columns(2)
+    with left_column:
+        if st.sidebar.button("Start Gallery"):
+            picResult = picGen(1)
+            picDisplay(picResult)
+        if st.sidebar.button("Next Page") :
+          st.session_state['start_index'] = 13
+          #start_index += 12
+          #st.write(st.session_state['start_index'])
+          picResult = []
+          picResult = picGen(st.session_state['start_index'])
+          picDisplay(picResult)
+        if st.sidebar.button('Previous Page') :
+          st.session_state['start_index'] = 1
+          #start_index = start_index - 12
+          #st.write(st.session_state['start_index'])
+          picResult = []
+          picResult = picGen(st.session_state['start_index'])
+          picDisplay(picResult)
+
+ 
