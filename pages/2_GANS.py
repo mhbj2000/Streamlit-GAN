e06e70a7cadd4fdbe069adc01656ad3402592285
GivanteLewis <112026471+GivanteLewis@users.noreply.github.com>
2022-11-20 18:02:34 -0500

    Update 2_GANS.py

--- pages/2_GANS.py
+++ pages/2_GANS.py
@@ -70,9 +70,9 @@ tab1,tab2,tab3 =st.tabs(['GAN Model Training','Clear To Rain Model Visualizer', 
 # ---- GAN Rain Visualizer | Row 1 ----         
 with tab1:
     with st.container():
-    col1, col2, col3,= st.columns(3)
-    with col2:
-        st.header('GAN Model Training')
+        col1, col2, col3,= st.columns(3)
+        with col2:
+            st.header('GAN Model Training')
     image_resizer = (700,425)
     col1,col2 = st.columns(2)
     with col2:
