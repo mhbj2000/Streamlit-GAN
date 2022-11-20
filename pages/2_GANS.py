5fa7788280b6b02dabc0eef8a7e5fcb69fa9080d
GivanteLewis <112026471+GivanteLewis@users.noreply.github.com>
2022-11-20 18:18:46 -0500

    Update 2_GANS.py

--- pages/2_GANS.py
+++ pages/2_GANS.py
@@ -94,15 +94,15 @@ with tab1:
         #st.write('Time to Train: 10.5 hrs approx.')
         image_counter = st.slider('Example Image Display Slider',1,5,1,key = 'clear_image slider',label_visibility ='visible')
         #clear_or_rainy = st.selectbox('Choose a Dataset',('Clear Image Dataset','Rainy Image Dataset','Foggy Image Dataset'))
-        if clear_or_rainy == 'Clear Image Dataset':
-            pass
+        #if clear_or_rainy == 'Clear Image Dataset':
+            #pass
             #st.write('Total Length of Clear Dataset: 1013 Images')
             #st.markdown('<p class="main-text">Total Length of Clear Dataset: 1013 Images <br>', unsafe_allow_html=True) 
-        elif clear_or_rainy == 'Rainy Image Dataset':
-            pass
+        #elif clear_or_rainy == 'Rainy Image Dataset':
+            #pass
             #st.write('Total Length of Rainy Dataset: 1054 Images')
-        elif clear_or_rainy == 'Foggy Image Dataset':
-            pass
+        #elif clear_or_rainy == 'Foggy Image Dataset':
+            #pass
             #st.write('Total Length of Foggy Dataset: 1008 Images')
  
     with col1:
