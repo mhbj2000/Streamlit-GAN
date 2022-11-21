8d3cf5890379ff7f6115b876111cea91d4f5e8b7
GivanteLewis <112026471+GivanteLewis@users.noreply.github.com>
2022-11-21 13:32:48 -0500

    Update 2_GANS.py

--- pages/2_GANS.py
+++ pages/2_GANS.py
@@ -161,7 +161,7 @@ with tab2:
     with col2:                 
         #st.markdown('<p class="subheader">Choose an Example</p>', unsafe_allow_html=True)
         #model_selector = st.selectbox('Choose an Example',('Example 1', 'Example 2', 'Example 3'),label_visibility='collapsed')
-        st.markdown('<p class="subheader">Image Iterations</p>', unsafe_allow_html=True)
+        #st.markdown('<p class="subheader">Image Iterations</p>', unsafe_allow_html=True)
         #rain_counter = st.slider('Intermediate Image Number', 0,6,label_visibility="collapsed") 
     with col1:
         if model_selector == 'Example 1':
