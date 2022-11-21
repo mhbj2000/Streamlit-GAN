b259b53d6ba0a339366bfc4889e1932da4f88a7a
GivanteLewis <112026471+GivanteLewis@users.noreply.github.com>
2022-11-21 13:28:44 -0500

    Update 2_GANS.py

--- pages/2_GANS.py
+++ pages/2_GANS.py
@@ -141,13 +141,11 @@ with tab2:
     with col2:
         st.markdown('<p class="subheader">Choose an Example</p>', unsafe_allow_html=True)
         model_selector = st.selectbox('Choose an Example',('Example 1', 'Example 2', 'Example 3'),label_visibility='collapsed')
-        
-    my_col1,my_col2,my_col3 = st.columns(3)
-    #with my_col1:
-        #model_selector = st.selectbox('Choose an Example',('Example 1', 'Example 2', 'Example 3'),label_visibility='collapsed')
-    
-    with my_col3:
+        st.markdown('<p class="subheader">Image Iterations</p>', unsafe_allow_html=True)
         rain_counter = st.slider('Intermediate Image Number', 0,6,label_visibility="collapsed")
+
+ 
+    
         
     #with tab_container:
         #with my_col1:
