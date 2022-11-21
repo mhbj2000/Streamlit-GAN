318605fd928d0b4f3e67ae31d6a95f05986dfd8a
GivanteLewis <112026471+GivanteLewis@users.noreply.github.com>
2022-11-20 23:21:39 -0500

    Update 2_GANS.py

--- pages/2_GANS.py
+++ pages/2_GANS.py
@@ -201,7 +201,7 @@ with tab2:
         with col2:
             
             #st.markdown('<p class="headers">Intermediate Images</p>', unsafe_allow_html=True)
-            image_resizer = (350,350)
+            image_resizer = (400,400)
             rain_timelapse_picture = rain_timelapse_picture.resize(image_resizer)
             st.image(rain_timelapse_picture)
         
