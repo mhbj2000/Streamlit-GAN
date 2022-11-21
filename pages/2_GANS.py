ff2bd1660072180bc619893b056896cca68c1dc2
GivanteLewis <112026471+GivanteLewis@users.noreply.github.com>
2022-11-20 22:44:20 -0500

    Update 2_GANS.py

--- pages/2_GANS.py
+++ pages/2_GANS.py
@@ -140,7 +140,7 @@ with tab2:
         #st.markdown('<p class="headers">Controls and Metrics</p>', unsafe_allow_html=True)
         st.markdown('<p class="subheader">Choose an Example</p>', unsafe_allow_html=True)
         model_selector = st.selectbox('Choose an Example',('Example 1', 'Example 2', 'Example 3'),label_visibility='collapsed')
-        st.markdown('<p class="subheader">Intermediate Image Number</p>', unsafe_allow_html=True)
+        st.markdown('<p class="subheader">Iterations</p>', unsafe_allow_html=True)
         rain_counter = st.slider('Intermediate Image Number', 0,6,label_visibility="collapsed")            
         if model_selector == 'Example 1':
             if rain_counter == 0:
@@ -188,7 +188,7 @@ with tab3:
         st.markdown('<p class="headers">Controls and Metrics</p>', unsafe_allow_html=True)
         st.markdown('<p class="subheader">Choose an Example</p>', unsafe_allow_html=True)
         model_selector = st.selectbox('Choose an Example',('Example 1', 'Example 2', 'Example 3'), key = '2nd selectbox',label_visibility="collapsed")
-        st.markdown('<p class="subheader">Intermediate Image Number</p>', unsafe_allow_html=True)
+        st.markdown('<p class="subheader">Iterations</p>', unsafe_allow_html=True)
         fog_counter = st.slider('Intermediate Image Number', 0,6, key = '2nd slider',label_visibility="collapsed")
         if model_selector == 'Example 1':
             if fog_counter == 0:
