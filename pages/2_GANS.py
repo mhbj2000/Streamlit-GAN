6d675eb54a5c21a8d0fa6d29e6e51f0c9f3fefcb
GivanteLewis <112026471+GivanteLewis@users.noreply.github.com>
2022-11-21 23:39:51 -0500

    Update 2_GANS.py

--- pages/2_GANS.py
+++ pages/2_GANS.py
@@ -208,7 +208,7 @@ with tab3:
         st.video(video_bytes)
     with col2:
         #---Display image, resize, and label---
-        st.markdown('<p class="headers">Intermediate Images</p>', unsafe_allow_html=True)
+        st.markdown('<p class="headers">           Intermediate Images</p>', unsafe_allow_html=True)
         image_resizer = (400,400)
         fog_timelapse_picture = fog_timelapse_picture.resize(image_resizer)
         st.image(fog_timelapse_picture)        
