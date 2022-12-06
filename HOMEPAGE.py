dc840dab7f41494678df053de1e775dfdfe95035
TivonB <112200765+TivonB@users.noreply.github.com>
2022-12-05 19:50:12 -0500

    Update HOMEPAGE.py

--- HOMEPAGE.py
+++ HOMEPAGE.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 import base64
-st.caching.clear_cache()
+st.runtime.legacy_caching.clear_cache()
 
 st.set_page_config(page_title="GANS", layout="wide")
 
